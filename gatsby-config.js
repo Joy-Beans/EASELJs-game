f7ca410289aaf6015d2d24b0c13caf42c0fe1fd0
Joy-Beans <108127972+Joy-Beans@users.noreply.github.com>
2020-12-20 07:19:46 +0000

    Preparing release 1.3

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,3 +11,5 @@ module.exports = {
         name: 'Gatsby + Node.js (TypeScript) API',
         short_name: 'Gatbsy + Node.js (TypeScript)',
         start_url: '/',
+        icon: 'src/images/gatsby-icon.png',
+      },
